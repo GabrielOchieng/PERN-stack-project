f7d636a15ac79ce101c4a7ea4c1a10f3903f2e46
GabrielOchieng <ogingagabriel@gmail.com>
2024-04-22 12:26:35 +0300

    data fetching

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 
 const morgan = require("morgan");
 const app = express();
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
   try {
     const results = await db.query("select * from restaurants");
-    console.log(results);
+
     res.status(200).json({
       status: "success",
       results: results.rows.length,
